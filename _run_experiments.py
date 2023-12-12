666d6bbfe2902de2abe6b0a6cf88d22cfda7b1d0
John Winnicki <winnicki@DN51sle8.SUNet>
2023-12-12 12:42:25 -0800

    Deleted Comments

--- _run_experiments.py
+++ _run_experiments.py
@@ -246,107 +246,27 @@ def get_lr(it):
 # JOHN 12/09/2023
 
 def l1_unstructured_prune(module, name, amount):
-    """Apply L1 unstructured pruning to a specified parameter of a module,
-    following PyTorch's pruning convention.
-
-    Args:
-        module (nn.Module): The module containing the parameter to prune.
-        name (str): The name of the parameter within the module to prune.
-        amount (float): The fraction of connections to prune (between 0 and 1).
-
-    Raises:
-        ValueError: If the amount is not between 0 and 1.
-    """
-    # Ensure amount is a valid fraction
     if not 0 <= amount <= 1:
         raise ValueError("Pruning amount must be a fraction between 0 and 1.")
-
-    # Retrieve the tensor to prune
     tensor = getattr(module, name)
-
-    # Compute the number of parameters to prune
     num_params_to_prune = round(amount * tensor.nelement())
-
-    # Compute the mask
     if num_params_to_prune != 0:
         l1_norm = torch.abs(tensor.view(-1))
         threshold = torch.topk(l1_norm, num_params_to_prune, largest=False).values[-1]
         mask = torch.gt(l1_norm, threshold).float().view_as(tensor)
     else:
         mask = torch.ones_like(tensor)
-
-    # Save the original tensor and the mask
     module.register_parameter(name + "_orig", torch.nn.Parameter(tensor.detach()))
     module.register_buffer(name + "_mask", mask)
-
-    # Define the hook for applying the mask
     def apply_mask_hook(module, inputs):
         orig_tensor = getattr(module, name + "_orig")
         mask = getattr(module, name + "_mask")
         pruned_tensor = orig_tensor * mask
-        # Set the pruned tensor as a parameter temporarily for the forward pass
         setattr(module, name, torch.nn.Parameter(pruned_tensor))
-
-
-    # Attach the hook to the module
     hook = module.register_forward_pre_hook(apply_mask_hook)
-
     return hook
 
-
-
-# def l2_structured_pruning(model, current_layer_name, previous_layer_name, amount):
-#     """
-#     Apply L2 structured pruning to a specified linear layer of a model, 
-#     and adjust the dimensions of the previous linear layer accordingly.
-#     """
-#     print("\n\n\n\n")
-#     print(getattr(model, current_layer_name).in_features, getattr(model, current_layer_name).out_features)
-#     # print(getattr(model, previous_layer_name).in_features, getattr(model, previous_layer_name).out_features)
-#     current_layer = getattr(model, current_layer_name)
-#     # previous_layer = getattr(model, previous_layer_name)
-
-#     # if not (isinstance(current_layer, nn.Linear) and isinstance(previous_layer, nn.Linear)):
-#         # raise TypeError("Both layers must be of type nn.Linear.")
-
-#     # Compute L2 norm for each row in the weight matrix of the current layer
-#     l2_norm = torch.norm(current_layer.weight.data, p=2, dim=1)
-#     num_rows_to_keep = int((1 - amount) * l2_norm.size(0))
-#     rows_to_keep = torch.topk(l2_norm, num_rows_to_keep, largest=True).indices
-
-#     # print("aw09da09wdopjiawda098a6a5w5wa56wa", previous_layer.weight.data.shape, previous_layer.in_features)
-
-#     # Create new pruned layer for current_layer
-#     new_current_layer = nn.Linear(current_layer.in_features, num_rows_to_keep, bias=current_layer.bias is not None) #(384, 372)
-#     new_current_layer.weight.data = current_layer.weight.data[rows_to_keep, :]
-#     if current_layer.bias is not None:
-#         new_current_layer.bias.data = current_layer.bias.data[rows_to_keep]
-
-#     # # Adjust previous_layer to match the new dimension of current_layer
-#     # new_previous_layer = nn.Linear(num_rows_to_keep, previous_layer.out_features, bias=previous_layer.bias is not None)
-#     # new_previous_layer.weight.data = previous_layer.weight.data[:, rows_to_keep]
-
-#     print("aw0d9aiud09aiwk", current_layer.weight.shape)
-#     # print("aw0d9aiud09aiwk", previous_layer.weight.shape)
-
-#     print("aw0d9aiud09aiwk", new_current_layer.weight.shape)
-#     # print("aw0d9aiud09aiwk", new_previous_layer.weight.shape)
-
-#     # Replace the old layers with the new pruned layers in the model
-#     setattr(model, current_layer_name, new_current_layer)
-#     # setattr(model, previous_layer_name, new_previous_layer)
-#     print(getattr(model, current_layer_name).weight.shape)
-#     # print(getattr(model, previous_layer_name).weight.shape)
-    
-#     print(getattr(model, current_layer_name).in_features, getattr(model, current_layer_name).out_features)
-#     # print(getattr(model, previous_layer_name).in_features, getattr(model, previous_layer_name).out_features)
-
 def l2_structured_attn_pruning(model, name, name2, amount, n_head):
-    """
-    Apply L2 structured pruning to a specified linear layer of a model, 
-    and adjust the dimensions of the previous linear layer accordingly.
-    """
-
     c_attn_layer = getattr(model, name)
     # print("CPROJ ORIGINAL SHAPE: ", getattr(model, "c_proj").weight.data.shape)
     print("ORIGINAL: ", getattr(model, name).weight.data.shape)
@@ -375,7 +295,6 @@ def l2_structured_attn_pruning(model, name, name2, amount, n_head):
     # print("NEW: ", n_head, vnum_rows_to_keep)
     vrows_to_keep = torch.topk(vl2_norm, vnum_rows_to_keep, largest=True).indices
 
-    # Create new pruned layer for current_layer
     # print("BIAS: ", c_attn_layer.bias)
     new_current_layer = nn.Linear(c_attn_layer.in_features, qnum_rows_to_keep+knum_rows_to_keep+vnum_rows_to_keep, bias=c_attn_layer.bias is not None)
     new_current_layer.weight.data = torch.cat([q[qrows_to_keep, :] , k[krows_to_keep, :] , v[vrows_to_keep, :] ], dim=0)
@@ -386,50 +305,16 @@ def l2_structured_attn_pruning(model, name, name2, amount, n_head):
 
     c_proj_layer = getattr(model, name2)
 
-    # Adjust c_proj layer to match the new dimension of the pruned V part
-    # Since c_proj is a Linear layer with shape [n_embd, n_embd], and only the input dimension needs to change
     new_c_proj_layer = nn.Linear(vnum_rows_to_keep, c_proj_layer.out_features, bias=c_proj_layer.bias is not None)
 
-    # Copy the relevant weights and bias from the original c_proj layer to the new pruned layer
-    # We take the columns corresponding to the kept rows in V
     new_c_proj_layer.weight.data = c_proj_layer.weight.data[:, vrows_to_keep]
     if c_proj_layer.bias is not None:
         new_c_proj_layer.bias.data = c_proj_layer.bias.data[vrows_to_keep]
 
-    # Replace the old c_proj layer with the new pruned layer in the model
     setattr(model, name2, new_c_proj_layer)
     
     # print(getattr(model, name2).in_features, getattr(model, name2).out_features, getattr(model, name2).weight.data.shape)
 
-
-
-
-
-# def find_linear_pairs(module):
-#     """
-#     Find pairs of consecutive linear layers in a given module.
-
-#     Args:
-#         module (torch.nn.Module): The module (or block) to search through.
-
-#     Returns:
-#         List of tuples: Each tuple contains (previous_layer_name, current_layer_name).
-#     """
-#     linear_pairs = []
-#     previous_module = None
-#     previous_name = None
-
-#     # Iterate through all submodules and their names within the given module
-#     for name, submodule in module.named_modules():
-#         # Check if both the current and previous submodules are linear layers
-#         if isinstance(previous_module, nn.Linear) and isinstance(submodule, nn.Linear):
-#             linear_pairs.append((previous_name, name))
-        
-#         # Update the previous module and name
-#         previous_module = submodule
-#         previous_name = name
-
-#     return linear_pairs
 
 def apply_unstructured_pruning_to_causal_self_attention(module, pruning_percentage=0.2):
     l1_unstructured_prune(module.c_attn, name='weight', amount=pruning_percentage)
@@ -438,52 +323,25 @@ def apply_structured_pruning_to_causal_self_attention(block, pruning_percentage=
     l2_structured_attn_pruning(block, 'c_attn', 'c_proj', pruning_percentage, n_head)
 
 def prune_gpt_model(model, pruning_percentage=0.2):
-    # Iterate over all blocks in the transformer and apply pruning to the CausalSelfAttention layers
     for block in model.transformer.h:
         apply_unstructured_pruning_to_causal_self_attention(block.attn, pruning_percentage)
         # apply_structured_pruning_to_causal_self_attention(block.attn, pruning_percentage)
 
 def calculate_reduction_percentage(current_percentage, target_percentage):
-    """
-    Calculate the percentage reduction needed to go from the current percentage
-    of the original size to the target percentage of the original size.
-
-    :param current_percentage: Current size as a percentage of the original size.
-    :param target_percentage: Desired size as a percentage of the original size.
-    :return: Reduction percentage required.
-    """
-    # Calculate the reduction factor
     reduction_factor = target_percentage / current_percentage
-
-    # Convert the reduction factor to a percentage reduction
     reduction_percentage = (1 - reduction_factor) * 100
-
     return reduction_percentage
 
 def calculate_percentage_to_reduce_each_step(start_percentage, step_reduction, steps):
-    """
-    Calculate the percentage to reduce at each step to decrease by a fixed percentage each time.
-
-    :param start_percentage: Starting percentage of the original size.
-    :param step_reduction: Fixed percentage reduction to achieve at each step.
-    :param steps: Number of steps for which the reduction is to be calculated.
-    :return: List of percentages to reduce at each step.
-    """
     percentages_to_reduce = []
     current_percentage = start_percentage
-
     for _ in range(steps):
-        # Calculate the target percentage for the next step
         target_percentage = current_percentage - step_reduction
-        # Calculate the percentage to reduce at this step
         reduction_percentage = calculate_reduction_percentage(current_percentage, target_percentage)
         percentages_to_reduce.append(reduction_percentage)
-        # Update the current percentage
         current_percentage = target_percentage
-
     return percentages_to_reduce
 
-# Calculate the percentage to reduce at each step to decrease by 10% each time
 percentages_to_reduce_each_step = calculate_percentage_to_reduce_each_step(100, 10, 10)  # 10 steps
 
 #################################################################
@@ -493,22 +351,18 @@ percentages_to_reduce_each_step = calculate_percentage_to_reduce_each_step(100, 
 #######################################################
 #JOHN INFERENCE FUNCTION: 12/11/2023
 def inferMe(model):
-    # -----------------------------------------------------------------------------
-    # Inference
-    # -----------------------------------------------------------------------------
-    init_from = 'gpt2' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
-    out_dir = 'out' # ignored if init_from is not 'resume'
-    start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-    num_samples = 1 # number of samples to draw
-    max_new_tokens = 500 # number of tokens generated in each sample
-    temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
-    top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
+    init_from = 'gpt2' 
+    out_dir = 'out'
+    start = "\n" 
+    num_samples = 1
+    max_new_tokens = 500
+    temperature = 0.8 
+    top_k = 200 
     seed = 1337
-    device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
-    dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
-    compile = False # use PyTorch 2.0 to compile the model to be faster
-    exec(open('configurator.py').read()) # overrides from command line or config file
-    # -----------------------------------------------------------------------------
+    device = 'cuda' 
+    dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' 
+    compile = False 
+    exec(open('configurator.py').read())
 
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
@@ -518,9 +372,7 @@ def inferMe(model):
     ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
     ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
-    # model
     if init_from == 'resume':
-        # init from a model saved in a specific directory
         ckpt_path = os.path.join(out_dir, 'ckpt.pt')
         checkpoint = torch.load(ckpt_path, map_location=device)
         gptconf = GPTConfig(**checkpoint['model_args'])
@@ -532,15 +384,13 @@ def inferMe(model):
                 state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
         model.load_state_dict(state_dict)
     elif init_from.startswith('gpt2'):
-        # init from a given GPT-2 model
         model = GPT.from_pretrained(init_from, dict(dropout=0.0))
 
     model.eval()
     model.to(device)
     if compile:
-        model = torch.compile(model) # requires PyTorch 2.0 (optional)
+        model = torch.compile(model)
 
-    # look for the meta pickle in case it is available in the dataset folder
     load_meta = False
     if init_from == 'resume' and 'config' in checkpoint and 'dataset' in checkpoint['config']: # older checkpoints might not have these...
         meta_path = os.path.join('data', checkpoint['config']['dataset'], 'meta.pkl')
@@ -549,24 +399,19 @@ def inferMe(model):
         # print(f"Loading meta from {meta_path}...")
         with open(meta_path, 'rb') as f:
             meta = pickle.load(f)
-        # TODO want to make this more general to arbitrary encoder/decoder schemes
         stoi, itos = meta['stoi'], meta['itos']
         encode = lambda s: [stoi[c] for c in s]
         decode = lambda l: ''.join([itos[i] for i in l])
     else:
-        # ok let's assume gpt-2 encodings by default
         # print("No meta.pkl found, assuming GPT-2 encodings...")
         enc = tiktoken.get_encoding("gpt2")
         encode = lambda s: enc.encode(s, allowed_special={"<|endoftext|>"})
         decode = lambda l: enc.decode(l)
-
-    # encode the beginning of the prompt
     if start.startswith('FILE:'):
         with open(start[5:], 'r', encoding='utf-8') as f:
             start = f.read()
     start_ids = encode(start)
 
-    # function to measure inference latency
     def measure_inference_latency(batch_size):
         x = torch.tensor(start_ids * batch_size, dtype=torch.long, device=device).reshape(batch_size, -1)
         with torch.no_grad():
@@ -641,7 +486,6 @@ while True:
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
     
-    # evaluate the loss on train/val sets and write checkpoints
     #JOHN 12/11/2023: COMMENTED OUT THIS PART SINCE UNNECESSARY
     if iter_num % eval_interval == 0 and master_process:
         losses = estimate_loss()
@@ -670,8 +514,6 @@ while True:
     if iter_num == 0 and eval_only:
         break
     
-    # forward backward update, with optional gradient accumulation to simulate larger batch size
-    # and using the GradScaler if data type is float16
     for micro_step in range(gradient_accumulation_steps):
         if ddp:
             # in DDP training we only need to sync gradients at the last micro step.
